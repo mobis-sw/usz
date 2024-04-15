7f6cf9e94127419e38577aacb9780eb8c9f55224
Sujin Yu <86346615+zinee-u@users.noreply.github.com>
2024-04-15 12:48:29 +0900

    DFS 네트워크

--- prg43162.py
+++ prg43162.py
@@ -1,20 +1,23 @@
 #43162
 
 def solution(n, computers):
+    # 1<= n <= 200; O(N^2)
+    # computers; adjacency matrix
     answer = 0
-    
     visited = [0] * n
-
-    def dfs(computers, visited, now):
-        visited[now] = 1
-        for v in range(len(computers)):
-            if visited[v] == 0 and computers[now][v] == 1:
-                dfs(computers, visited, v)
     
+    def dfs(cur):
+        nonlocal computers, visited
+        for v in range(len(computers)):
+            if visited[v] == 0 and computers[cur][v] == 1:
+                visited[cur] = 1
+                dfs(v)
+    
+    # main
     while False in visited:
         for i in range(n):
             if visited[i] == 0:
-                dfs(computers, visited, i)
+                dfs(i)
                 answer += 1
             
     return answer
